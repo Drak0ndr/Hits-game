b2e7bc5f9f4cf631f61b727402ce587c79289770
Максим <drakondr.video@gmail.com>
2024-06-01 12:52:31 +0700

    add chunk

---
+++ Assets/World/ChunkRenderer.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ChunkRenderer : MonoBehaviour
+{
+    public const int ChunkWidth = 10;
+    public const int ChunkHeight = 128;
+    public int[,,] Blocks = new int[ChunkWidth, ChunkHeight, ChunkWidth];
+    // Start is called before the first frame update
+    private List<Vector3> vecticies = new List<Vector3>();
+    private List<int> triangles = new List<int>();
+    void Start()
+    {
+        Mesh chunkMesh = new Mesh();
+
+        Blocks = TerrainGenerator.GenerateTerrain((int)transform.position.x, (int)transform.position.z);
+        for (int y = 0; y < ChunkHeight; y++)
+        {
+            for (int x = 0; x < ChunkWidth; x++)
+            {
+                for (int z = 0; z < ChunkWidth; z++)
+                {
+                    GenerateBlock(x, y, z);
+                }
+            }
+        }
+
+
+
+
+        chunkMesh.vertices = vecticies.ToArray();
+        chunkMesh.triangles = triangles.ToArray();
+
+        chunkMesh.RecalculateNormals();
+        chunkMesh.RecalculateBounds();
+
+        GetComponent<MeshFilter>().mesh = chunkMesh;
+    }
+
+    private void GenerateBlock(int x, int y, int z)
+    {
+        var blockPosition = new Vector3Int(x, y, z);
+        if (GetBlockAtPosition(blockPosition) == 0) return;
+
+
+        if (GetBlockAtPosition(blockPosition + Vector3Int.right) == 0) GenerateRightSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.left) == 0) GenerateLeftSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.forward) == 0) GenerateFrontSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.back) == 0) GenerateBackSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.up) == 0) GenerateTopSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.down) == 0) GenerateBottomSide(blockPosition);
+    }
+
+    private int GetBlockAtPosition(Vector3Int blockPosition)
+    {
+        if (blockPosition.x >= 0 && blockPosition.x < ChunkWidth &&
+        blockPosition.y >= 0 && blockPosition.y < ChunkHeight &&
+        blockPosition.z >= 0 && blockPosition.z < ChunkWidth)
+        {
+            return Blocks[blockPosition.x, blockPosition.y, blockPosition.z];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    private void GenerateRightSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(1, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 0, 1) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 1) + blockPosition);
+
+        AddLastVerticiesSquare();
+
+    }
+
+    private void GenerateLeftSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(0, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(0, 0, 1) + blockPosition);
+        vecticies.Add(new Vector3(0, 1, 0) + blockPosition);
+        vecticies.Add(new Vector3(0, 1, 1) + blockPosition);
+
+        AddLastVerticiesSquare();
+    }
+
+    private void GenerateFrontSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(0, 0, 1) + blockPosition);
+        vecticies.Add(new Vector3(1, 0, 1) + blockPosition);
+        vecticies.Add(new Vector3(0, 1, 1) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 1) + blockPosition);
+
+        AddLastVerticiesSquare();
+    }
+
+    private void GenerateBackSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(0, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(0, 1, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 0) + blockPosition);
+
+        AddLastVerticiesSquare();
+    }
+
+    private void GenerateTopSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(0, 1, 0) + blockPosition);
+        vecticies.Add(new Vector3(0, 1, 1) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 1, 1) + blockPosition);
+
+        AddLastVerticiesSquare();
+    }
+
+    private void GenerateBottomSide(Vector3Int blockPosition)
+    {
+        vecticies.Add(new Vector3(0, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(1, 0, 0) + blockPosition);
+        vecticies.Add(new Vector3(0, 0, 1) + blockPosition);
+        vecticies.Add(new Vector3(1, 0, 1) + blockPosition);
+
+        AddLastVerticiesSquare();
+    }
+    private void AddLastVerticiesSquare()
+    {
+        triangles.Add(vecticies.Count - 4);
+        triangles.Add(vecticies.Count - 3);
+        triangles.Add(vecticies.Count - 2);
+
+        triangles.Add(vecticies.Count - 3);
+        triangles.Add(vecticies.Count - 1);
+        triangles.Add(vecticies.Count - 2);
+    }
+}
