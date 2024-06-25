213e59ccdb5753854836d3cc4951314b557637fe
Максим <drakondr.video@gmail.com>
2024-06-25 18:29:58 +0700

    add quality setting

---
+++ Assets/MainMenu/Settings.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class Settings : MonoBehaviour
+{
+    public TMP_Dropdown qualityDropdown;
+
+    public void Start() {
+        loadSettings();
+        qualityDropdown.value = 6;
+        PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+    }
+    public void setQuality() {
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
+    }
+
+    public void saveSettings() {
+        PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+    }
+
+    public void loadSettings() {
+        if(PlayerPrefs.HasKey("QualitySettingPreference")) {
+            qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
+        } else {
+            qualityDropdown.value = 6;
+            PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value);
+        }
+    }
+}
